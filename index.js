4f9fe24de0dbb582bba43875ee2abea2c9329c44
Axel Jamal <axeltaj@gmail.com>
2023-03-03 18:54:39 +0000

    Created an array for team, function to start-app, prompt to add employee

--- index.js
+++ index.js
@@ -10,6 +10,43 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+teamArr = [];
+
+function startApp() {
+  function buildTeam() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Which type of employee would you like to add to your team?",
+          name: "addEmployeePrompt",
+          choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+            "[Done adding employee] Enter this to build team",
+          ],
+        },
+      ])
+      .then(function (userInput) {
+        switch (userInput.addEmployeePrompt) {
+          case "Manager":
+            addManager();
+            break;
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+
+          default:
+            htmlBuilder();
+        }
+      });
+  }
+}
+
+startApp();
