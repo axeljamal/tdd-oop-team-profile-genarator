bf60a0b938f485c64d5cc277c795606c7b1bb7b6
Axel Jamal <axeltaj@gmail.com>
2023-03-03 19:02:32 +0000

    Added each inquirer.prompt to employee type

--- index.js
+++ index.js
@@ -47,6 +47,137 @@ function startApp() {
         }
       });
   }
+
+  // Employee prompts
+
+  function addManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the manager's name?",
+        },
+
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the manager's employee ID number?",
+        },
+
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the manager's email address?",
+        },
+
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is the manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+        teamArr.push(manager);
+        console.log("You have added ");
+        buildTeam();
+      });
+  }
+
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+        },
+
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's employee ID number?",
+        },
+
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email address?",
+        },
+
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the engineer's GitHub username?",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        teamArr.push(engineer);
+        buildTeam();
+      });
+  }
+
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the intern's name?",
+        },
+
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the intern's employee ID number?",
+        },
+
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the intern's email address?",
+        },
+
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What school does the intern attend?",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamArr.push(intern);
+        buildTeam();
+      });
+  }
+
+  function htmlBuilder() {
+    console.log("Succesfully built Team!");
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    fs.writeFileSync(outputPath, render(teamArr), "UTF-8");
+  }
+
+  buildTeam();
 }
 
 startApp();
